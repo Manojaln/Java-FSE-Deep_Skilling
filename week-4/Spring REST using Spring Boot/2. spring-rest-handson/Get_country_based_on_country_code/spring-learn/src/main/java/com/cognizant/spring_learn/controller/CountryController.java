c8265624a96ecfd790deeefa9643f7f8a5f22678
Manojaln <160051943+Manojaln@users.noreply.github.com>
2025-07-13 13:09:12 +0530

    Add files via upload
    Task completed i.e, to fetch country from an xml based on the country code and listen it on the given port as 8083

---
+++ week-4/Spring REST using Spring Boot/2. spring-rest-handson/Get_country_based_on_country_code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) throws Exception {
+        LOGGER.info("START: getCountry with code = {}", code);
+        return countryService.getCountry(code);
+    }
+}

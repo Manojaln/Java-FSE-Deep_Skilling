a291f2a0c198758c52aebb5b91c6fdd0e09337ad
Manojaln <160051943+Manojaln@users.noreply.github.com>
2025-07-27 23:25:45 +0530

    Add files via upload
    first additional handson completed

---
+++ week-6/React/6. ReactJS-HOL/trainersapp/src/TrainerMock.js
@@ -0,0 +1,7 @@
+import Trainer from './Trainer';
+const TrainerMock = [
+    new Trainer('A-Alan', 'Alan', 'alan@cognizant.com', '1234567890', 'Java ', ['SpringBoot', 'Microservices', 'ReactJS']),
+    new Trainer('B-Bob', 'Bob', 'bob@cognizant.com', '1234567890', 'Python', ['Django', 'SQL', 'Angular']),
+    new Trainer('C-Chandru', 'Chandru', 'chandru@cognizant.com', '1234567890', 'Java ', ['NodeJS', 'Express', 'ReactJS']),
+]
+export default TrainerMock;
